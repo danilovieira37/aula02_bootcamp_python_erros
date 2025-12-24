9d0d79c1d901a10e5b4f3e18029fe3eeaebb610d
danilovieira37 <danilovieira37@icloud.com>
2025-12-24 18:53:19 -0300

    adicionando exercicio 1

---
+++ exercicios_github.py
@@ -0,0 +1,4 @@
+# Exercício 01
+# Crie programa que o usuário digita o seu nome e retorna o número de caracteres
+nome = input('Por favor digite um nome para que eu possa calcular a quantidade de caracteres: ')
+print(f'O nome digitado possui {len(nome)} caracteres.')
